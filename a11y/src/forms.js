5cf55f1d2decea1ea3be6a366293184f4f1a03df
richNovoa <rich@cross.team>
2020-07-31 09:02:51 -0400

    react router tests

---
+++ a11y/src/forms.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+class Form extends React.Component {
+    render() {
+        return (
+            <div>
+                    <h1>Sign Up</h1>
+                    <h2>Please fill up this form to create an account</h2>
+              <form id="form1">
+                <p>
+                  <label for="firstname">First Name:</label>
+                  <input type="text" id="firstname" name="firstname"></input>
+                </p>
+                <p>
+                  <label for="lastname">Last Name:</label>
+                  <input type="text" id="lastname" name="lastname"></input>
+                </p>
+                <p>
+                  <label for="email"> Email: </label>
+                  <input type="text" id="email" name="email"></input>
+                </p>
+                <p>
+                <label for="password"> Password: </label>
+                <input type="text" id="password" name="password"></input>
+                </p>
+                <p>
+                <label for="cpassword"> Confirm Password: </label>
+                <input type="text" id="cpassword"  name="cpassword"></input>
+                </p>
+              <button type="submit" form="form1" value="submit" aria-label="sign up button">Sign Up</button>    
+            </form>
+          </div>
+        );
+    }
+}
+
+export default Form;
---
+++ a11y/src/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Accessibility Library</h2>
+        <p>This is a project aimed at applying accesibility best practices to ui components using react.</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
